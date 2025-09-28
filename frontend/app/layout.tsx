76d5d1bf1bff9938716f9b620eed0ce98d592a9b
Francesco Delmonaco <francescodelmonaco1999@gmail.com>
2025-09-28 18:20:16 +0200

    create next js project

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Alberi di Vita OdV",
+  description: "Organizzazione di volontariato a Brescia. Agisce in favore della terza età, con iniziative di aggregazione per grandi e piccini.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
